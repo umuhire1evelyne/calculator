a588c5119b67e248340cf5cb8a12b5cf5f0875fc
umuhire1evelyne <umuhire1evelyne@gmail.com>
2019-07-16 12:45:46 +0200

    i am adding some elements

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ function dis(val)
 {
   document.getElementById("result").value+=val
 }
+function solve()
+{
+  let a=document.getElementById("result").value
+  let b=eval(a)
+  document.getElementById("result").value = b
+
+}
+function clr()
+{
+  document.getElementById("result").value = ""
+}
+
